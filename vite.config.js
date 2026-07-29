565b3f6f051394436fc4d9a802c0f0175ea62e22
Nafrin14 <nafrinnafrin49@gmail.com>
2026-07-29 20:43:04 +0530

    Fix build and add Netlify cache config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
 
   plugins:[
     react(),
     tailwindcss()
-  ]
+  ],
+
+  build:{
+    cssMinify:true,
+    minify:"esbuild",
+    sourcemap:false
+  }
 
 })
